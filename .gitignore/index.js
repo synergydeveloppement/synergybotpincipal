afe336f9d30f25dc4f80f875323ae440ab4ef574
synergydeveloppement <51673606+synergydeveloppement@users.noreply.github.com>
2019-06-10 20:05:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -42,6 +42,41 @@ if(message.author.bot) return;
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args)
+        
+    if(message.channel.type !== "text") { 
+        if(message.author.id !== "559155914087596073") {
+            if(message.author.id === "453892276629012481") {
+   
+           if (message.content.indexOf('!rep') === 0) {
+                var stringA = message.content.split(" ");
+                var cpt = 2;
+                var messageee = "";
+                var ident = "";
+                stringA.forEach(element => {
+    
+                    if (stringA[cpt] === undefined) {
+    
+                    } else {
+    
+    
+                ident = stringA[1];
+    
+                        messageee = messageee.concat(stringA[cpt] + " ");
+                    }
+                    cpt++;
+                });
+    
+                bot.users.find('id','' + ident + '').send(messageee);
+    
+            }
+    
+            } else {
+                bot.users.find("id", '453892276629012481').send("MP de "+ message.author + "("+message.author.username+") : " + message.content);
+                message.reply("Hey "+message.author.username+" tu viens de m'envoier un dm ? Pas de souci mais sache que je ne pourrai pas te répondre :)");
+            }
+    
+        }
+      }
 })
 bot.on("guildCreate", async guild => {
   let guildCreateChannel = bot.channels.get(`575029668055482368`);
