555c0e74ac28f1b736a138cbfa69e9c4f37dd221
synergydeveloppement <51673606+synergydeveloppement@users.noreply.github.com>
2019-06-10 21:22:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,38 +13,6 @@ bot.on("ready",  () => {
     });
 })
 
-bot.commands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Aucun fichier(s) trouvé");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} chargé !`);
-    bot.commands.set(props.help.ame, props);
-  });
-
-});
-
-bot.on("message", message => {
-if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args)
-       
-})
-
 bot.on("message", message => {
 if(message.channel.type !== "text") { 
         if(message.author.id !== "559155914087596073") {
@@ -93,4 +61,37 @@ let joinEmbed = new Discord.RichEmbed()
     return guildCreateChannel.send(joinEmbed);
 
 });
+
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Aucun fichier(s) trouvé");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} chargé !`);
+    bot.commands.set(props.help.ame, props);
+  });
+
+});
+
+bot.on("message", message => {
+if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args)
+       
+})
+
 bot.login(process.env.TOKEN)
